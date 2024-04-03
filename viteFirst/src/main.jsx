ed435e715677dceff6d446e9eae15ec0e08e8536
showqi <mshowqi12@gmail.com>
2024-04-04 01:12:52 +0600

    react rendaring

--- viteFirst/src/main.jsx
+++ viteFirst/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Hello from './App2.jsx'
+let reactelement=React.createElement(
+  'a',
+  {href:'https://www.google.com/',target:'_blank'},
+  'GOOGLE'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <Hello/>
-    
-  </React.StrictMode>,
+  reactelement
+  
 )
